b59546d53a510bf4f0a310bb301ab791db54591d
Upasana <manemupasana55@gmail.com>
2023-06-28 18:31:44 +0530

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Upasana Manem on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
